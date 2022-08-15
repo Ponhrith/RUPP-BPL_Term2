10d76bc1fd511c509457c2fc03e063327051d730
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-15 08:31:31 +0700

    Comments Added

--- Exercise5.cpp
+++ Exercise5.cpp
@@ -18,44 +18,32 @@ int main(){
 
    Bank0.Bank = "0";
    Bank0.Balance = 25;
-   Bank0.Loan1 = 100.5;
-   Bank0.Loan2 = 320.5;
+   Bank0.Loan1 = 100.5; //1
+   Bank0.Loan2 = 320.5; //4
    Bank0.Asset = Bank0.Balance + Bank0.Loan1 +Bank0.Loan2;
    //cout << Bank0.Asset;
 
    Bank1.Bank = "1";
    Bank1.Balance = 125;
-   Bank1.Loan1 = 85;
-   Bank1.Loan2 = 40;
+   Bank1.Loan1 = 85; //2
+   Bank1.Loan2 = 40; //3
    Bank1.Asset = Bank1.Balance + Bank1.Loan1 +Bank1.Loan2;
 
    Bank2.Bank = "2";
    Bank2.Balance = 175;
-   Bank2.Loan1 = 125;
-   Bank2.Loan2 = 75;
+   Bank2.Loan1 = 125; //0
+   Bank2.Loan2 = 75; //3
    Bank2.Asset = Bank2.Balance + Bank2.Loan1 +Bank2.Loan2;
 
    Bank3.Bank = "3";
    Bank3.Balance = 75;
-   Bank3.Loan1 = 125;
+   Bank3.Loan1 = 125; //0
    Bank3.Loan2 = 0;
    Bank3.Asset = Bank3.Balance + Bank3.Loan1 +Bank3.Loan2;
-
-   Bank1.Bank = "1";
-   Bank1.Balance = 125;
-   Bank1.Loan1 = 85;
-   Bank1.Loan2 = 40;
-   Bank1.Asset = Bank1.Balance + Bank1.Loan1 +Bank1.Loan2;
-
-   Bank2.Bank = "2";
-   Bank2.Balance = 175;
-   Bank2.Loan1 = 125;
-   Bank2.Loan2 = 75;
-   Bank2.Asset = Bank2.Balance + Bank2.Loan1 +Bank2.Loan2;
-
+  
    Bank4.Bank = "4";
    Bank4.Balance = 181;
-   Bank4.Loan1 = 125;
+   Bank4.Loan1 = 125; //2
    Bank4.Loan2 = 0;
    Bank4.Asset = Bank4.Balance + Bank4.Loan1 +Bank4.Loan2;
 
