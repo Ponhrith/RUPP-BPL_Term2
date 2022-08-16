e2c5a6d112d3e6bc82754bb9c130aa591628ce02
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-16 20:26:56 +0700

    Testing

---
+++ Exercise2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    int temp,rev,rem,b;
+	// loop from 0 until 2002
+   	for(int i = 0; i <= 2002; i++){
+      	temp=i;
+      	rev=0;
+      	// find a reverse number
+      	while(temp){
+         	rem=temp%10;
+         	temp=temp/10;
+         	rev=rev*10+rem;
+      	}
+      	// print even number
+      	if(i==rev){
+      		if(i % 2 == 0){
+      			cout << " " << i;
+      			b++;
+    			if (b%10==0){
+    				cout << endl;
+    			}
+    
+  			}
+    	}
+         
+   }
+    return 0;
+}
