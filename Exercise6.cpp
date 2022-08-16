3e69cb0359771be23a3a0b4e25d989c815135eec
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-16 20:25:43 +0700

    Initial Commit

---
+++ Exercise6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    int whitePearl[1] = {1};
+    int mixPearls[2][5] = {
+        {1, 2, 3, 4},
+        {1,2,3,4,5,},
+    };
+
+
+}
