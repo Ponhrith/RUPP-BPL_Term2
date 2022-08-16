b34465dc08d246ab1b74f26c6f921bdc79236876
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-16 23:28:46 +0700

    Done

--- Exercise6.cpp
+++ Exercise6.cpp
@@ -1,13 +1,95 @@
 #include <iostream>
-#include <cstdlib>
+#include <ctime>
+#include <string>
+
 using namespace std;
 
-int main(){
-    int whitePearl[1] = {1};
-    int mixPearls[2][5] = {
-        {1, 2, 3, 4},
-        {1,2,3,4,5,},
-    };
+    int main() {
 
+        srand(time(0));
 
-}
+        int sim = 0, life = 100;
+
+        int picker, bowl;
+        int w_count,b_count;
+
+        int bowl_1[9] = {};
+        int bowl_1_index = 0;
+
+        int bowl_2[9] = {};
+        int bowl_2_index = 0;
+
+        while ( sim < 100 ) {
+            sim ++;
+
+            bowl_1_index = 0;
+            bowl_2_index = 0;
+            w_count = 0, b_count = 0;
+
+            for (int pick = 0; pick < 10; pick++) {
+                picker = rand() % 2;
+                bowl  = rand() % 2;
+
+                if (picker == 0) {
+                    b_count++;
+                }
+                else {
+                    w_count++;
+                }
+                if (b_count > 5) {
+                    picker = 1;
+                }
+                if (w_count > 5) {
+                    picker = 0;
+                }
+
+                if (bowl_1_index == 8) {
+                    bowl_2[bowl_2_index] = picker;
+                    bowl_2_index++;
+
+                }
+                if (bowl_2_index == 8) {
+                    bowl_1[bowl_1_index] = picker;
+                    bowl_1_index++;
+
+                }
+
+                if (bowl == 0) {
+                    bowl_1[bowl_1_index] = picker;
+                    bowl_1_index++;
+
+                }
+                else {
+                    bowl_2[bowl_2_index] = picker;
+                    bowl_2_index++;
+
+                }  
+            }
+
+            int hand_pick, picked;
+            int bowl_picker  = rand() % 2;
+
+            if (bowl_picker == 0) {
+                hand_pick = rand() % bowl_1_index;
+                picked = bowl_1[hand_pick];
+                
+            } 
+            else {
+                hand_pick = rand() % bowl_2_index;
+                picked = bowl_2[hand_pick];
+
+            }
+
+            if (picked == 0) {
+                // dead
+                life--;
+                
+            }
+        }
+        
+        std::cout << "Simulation:\n";
+        std::cout << "Death: " << (100 - life) << " Times\n";
+        std::cout << "Survived: " << life << " Times\n";
+
+        return 0;
+    }
--- Exercise7.cpp
+++ Exercise7.cpp
@@ -9,47 +9,24 @@ int main(){
  srand(time(0));
 
  bool dead = false;
- int round = 0;
-
- cout << "Russian Roulette" << endl;
+ 
+ cout << "exercise 7" << endl;
  
  int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
   
   for(int i = 0; i <  1000; i++){
   for(int i = 0; i < 5; i++) {
-   int random = rand() % 6;
+   int random = rand() % 7;
   
-        
-   
-     if(random == 1){
-       dead = true;
-       a++;}
-
-      else if(random == 2){
-       dead = true;
-       b++;}
-
-      else if(random == 3){
-       dead = true;
-       c++;}
-
-      else if(random == 4){
-       dead = true;
-       d++;}
-
-      else if(random == 5){
-       dead = true;
-       e++;}
-
-      else if(random == 6){
-       dead = true;
-        f++;}
-    
- }
-  }
-
- 
-  cout << "player 1 got bullet "  << a << endl;
+    if(random == 1){ a++;}
+    else if(random == 2){b++;}
+    else if(random == 3){c++;}
+    else if(random == 4){d++;}
+    else if(random == 5){e++;}
+    else if(random == 6){f++;}
+  }   
+  }   
+   cout << "player 1 got bullet "  << a << endl;
   cout << "player 2 got bullet "  << b << endl;
   cout << "player 3 got bullet "  << c << endl;
   cout << "player 4 got bullet "  << d << endl;
@@ -58,4 +35,3 @@ int main(){
 
   return 0;
 }
-
