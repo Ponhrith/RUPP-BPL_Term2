afc4ddf2bcd4a22459c94530e6c5a5a7931ec231
Ponhrith <ponhrith.nhep@gmail.com>
2022-08-17 09:07:45 +0700

    Testing

--- Exercise1.cpp
+++ Exercise1.cpp
@@ -4,20 +4,23 @@ int main()
 {  
 int number, sum = 0 , m, digitnumber = 0, average; 
  
-
+//ask user to input number
 cout  <<"Enter a number: ";    
 cin >>number; 
 
 while (number  > 0)    
 {    
-m = number%10;    
+    
+m = number%10;  //divide the number into element and sum all element 
 sum = sum+m;    
 number = number/10; 
-digitnumber++;   
+
+digitnumber++;  // count the number digit 
 }    
 
 
-average = sum / digitnumber;
-cout << "The average is "  << average;
+average = sum / digitnumber;//find the average
+
+cout << "The average is "  << average;//display the output
 return 0;
 }
--- Exercise6.cpp
+++ Exercise6.cpp
@@ -8,14 +8,17 @@ using namespace std;
 
         srand(time(0));
 
+        // life and simulation count
         int sim = 0, life = 100;
 
+        // declare variables
         int picker, bowl;
         int w_count,b_count;
 
+        // bowl 1 and its pearl count
         int bowl_1[9] = {};
         int bowl_1_index = 0;
-
+        // bowl 2 and its pearl count
         int bowl_2[9] = {};
         int bowl_2_index = 0;
 
@@ -26,16 +29,23 @@ using namespace std;
             bowl_2_index = 0;
             w_count = 0, b_count = 0;
 
+
             for (int pick = 0; pick < 10; pick++) {
+                // 1= white, 0=black
                 picker = rand() % 2;
+
+                // 0= bowl 1, 1= bowl 2
                 bowl  = rand() % 2;
 
+                // which one we got and add count
                 if (picker == 0) {
                     b_count++;
                 }
                 else {
                     w_count++;
                 }
+
+                // make sure it only use 5 of each pearl
                 if (b_count > 5) {
                     picker = 1;
                 }
@@ -43,6 +53,7 @@ using namespace std;
                     picker = 0;
                 }
 
+                // make sure to not exceed maxinum pearl per bowl
                 if (bowl_1_index == 8) {
                     bowl_2[bowl_2_index] = picker;
                     bowl_2_index++;
@@ -54,6 +65,7 @@ using namespace std;
 
                 }
 
+                // add to bowl
                 if (bowl == 0) {
                     bowl_1[bowl_1_index] = picker;
                     bowl_1_index++;
@@ -66,9 +78,11 @@ using namespace std;
                 }  
             }
 
+            // pick pearl from bowl
             int hand_pick, picked;
             int bowl_picker  = rand() % 2;
 
+            // check which we got
             if (bowl_picker == 0) {
                 hand_pick = rand() % bowl_1_index;
                 picked = bowl_1[hand_pick];
@@ -79,14 +93,15 @@ using namespace std;
                 picked = bowl_2[hand_pick];
 
             }
-
+            // die, if get black pearl
             if (picked == 0) {
                 // dead
                 life--;
                 
             }
         }
-        
+
+        // diplay 
         std::cout << "Simulation:\n";
         std::cout << "Death: " << (100 - life) << " Times\n";
         std::cout << "Survived: " << life << " Times\n";
