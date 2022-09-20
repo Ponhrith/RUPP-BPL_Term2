9d8eed980f7bfa2a6a6326395445d21e178d2f67
Ponhrith <ponhrith.nhep@gmail.com>
2022-09-20 21:13:25 +0700

    Testing

--- Dynamic-Memory-Allocation/Exercise6.cpp
+++ Dynamic-Memory-Allocation/Exercise6.cpp
@@ -1,19 +1,29 @@
 #include<iostream>
+#include<cstdlib>
 using namespace std;
 int main() {
- int x, n;
- cout << "Enter the size of array:" << "\n";
+ // declare 4 integer variable
+ int x, n,consonants=0,vowels=0;
+ // User enter the value of variable n
+ cout << "Enter the size of array: ";
  cin >>n;
- char *arr = new char(n);
+ // declare an array to hold a total of n integers and assigning it to pointer variable *arr
+char *arr = (char*) malloc(100 * sizeof(char));
+ // print the message the user to enter n number of characters
  cout << "Enter " << n << " characters" << endl;
+ // for loop to createa a loop variable x to iterate over the character entered by the user and read the elements entered by the user and store them in the array arr
  for (x = 0; x < n; x++) { 
   cin >> arr[x];
 if(arr[x]=='a' || arr[x]=='e' || arr[x]=='i' || arr[x]=='o' || arr[x]=='u' || arr[x]=='A' || arr[x]=='E' || arr[x]=='I' || arr[x]=='O' || arr[x]=='U')
 {
-cout << endl ;
+vowels++;
 }
 else
-cout << "The consonant = " << arr[x]<<endl;
+{
+consonants++;
+cout << "The consonant = "<<consonants<<endl;
 }
+}
+ free(arr);
  return 0;
 }
