39055c777f1c10059e8e713cdf1a6b7773eccf36
Ponhrith <ponhrith.nhep@gmail.com>
2022-09-20 22:35:59 +0700

    Testing

--- Dynamic-Memory-Allocation/Exercise08.cpp
+++ Dynamic-Memory-Allocation/Exercise08.cpp
@@ -1,2 +1,37 @@
 #include <iostream>
-#include <string.h> 
+using namespace std;
+
+int main(){
+
+
+    string temp,n;
+	string *str = new string[10];
+    cout << "Enter 10 Countries: " << endl;
+    for(int i = 0; i < 10; ++i)
+    {
+    cin >> str[i];
+    }
+
+    // Use Bubble Sort to arrange words
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9 - i; ++j) {
+            if (str[j] > str[j + 1]) {
+                temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Name of the countries After Sorts: " << endl;
+
+    for(int i = 0; i < 10; ++i)
+    {
+       cout << str[i] << endl;
+    }
+
+    
+    // delete[]str;
+    free(str);
+    return 0;
+}
