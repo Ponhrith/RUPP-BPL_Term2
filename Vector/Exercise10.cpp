2c5eddad4a5bd2c3a37976eacb4427ef2f13c03a
Ponhrith <ponhrith.nhep@gmail.com>
2022-10-17 21:23:16 +0700

    Testing

--- Vector/Exercise10.cpp
+++ Vector/Exercise10.cpp
@@ -1,32 +1,48 @@
 #include <iostream>
-#include <string>
-#include <sstream>
 #include <vector>
+#include <string>
+#include <cstring>
 using namespace std;
+int correct = 0;
 
-int findCountry(string arr[], int len, string seek)
-{
-    for (int i = 0; i < len; ++i)
-    {
-        if (arr[i] == seek) return i;
-    }
-    return -1;
-}
+vector <vector<string>> table = {{"Cambodia", "Thailand", "China", "Japan", "India", "Malaysia"},
+                                {"Phnom Penh", "Bangkok", "Beijing", "Tokyo", "Delhi", "Kuala Lumpur"}};
+
+void check(string capital, int index);
 
 int main(){
-    std::vector<std::string> countries={"Cambodia", "Thailand", "China", "Japan", "India", "Malaysia"};
-    std::vector<std::string> capital={"Phnom Penh", "Bangkok", "Beijing", "Tokyo", "Delhi", "Kuala Lumpur"};
-        
-    string country;
+
+
+    int index = 0;
+    string capital;
     
-    cout << "Country: ";
-    cin >> country;
+    do{
+        cout << "Enter a capital city for "<< table[0][index] <<": ";
+        getline(cin, capital);
+        check(capital, index);
+        index++;
+    }while(index != table[0].size());    
     
-    int index = findCountry(countries,5,country);
-     if (index >= 0)
-        cout << "Capital: " << capital[index];
-    else
-        cout << country << " is not a South East Asian Country";
-    
+    cout << "The correct count is "<< correct <<"";
+
     return 0;
+}
+
+void check(string capital, int index){
+
+    bool check = false;
+    
+    if(strcmpi(capital.c_str(), table[1][index].c_str()) == 0){
+        check = true;
+                
+    }
+
+    if(check == true){
+        cout << "Your answer is correct\n\n";
+        correct++;
+    }
+    else{
+        cout << "The correct capital should be "<< table[1][index] <<"\n\n";
+    }
+    
 }
