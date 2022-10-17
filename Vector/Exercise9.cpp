be512ceb979093d22176ed22af7bb3ec7c440333
Ponhrith <ponhrith.nhep@gmail.com>
2022-10-17 13:39:26 +0700

    Testing

---
+++ Vector/Exercise9.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+
+
+typedef std::pair<int, int> pair;
+
+int main()
+{
+	std::vector<pair> v = { {4, 2 }, {1, 7 }, {4, 5 }, {1, 2 }, {1, 1}, {4, 1 } };
+
+	std::sort(v.begin(), v.end(),
+			[](const pair &x, const pair &y) {
+				// compare the second value
+				if (x.second != y.second) {
+					return x.second < y.second;
+				}
+
+				// compare first only if the second value is equal
+				return x.first < y.first;
+			});
+
+	for (const pair &p: v) {
+		std::cout << '{' << p.first << ',' << p.second << '}' << std::endl;
+	}
+
+	return 0;
+}
